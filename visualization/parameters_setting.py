e6a1b60d6ec2d818b66212bfefe0e04d28ad9175
brucewan2046 <brucewan2046@163.com>
2021-09-30 10:40:47 +0800

    Update parameters_setting.py

--- visualization/parameters_setting.py
+++ visualization/parameters_setting.py
@@ -61,3 +61,22 @@ top_tick = [6,10,15]
 top_label = ['key','best','lowest']
 ax_top.set_xticks(top_tick)
 ax_top.set_xticklabels(top_label,rotation=90)
+
+
+
+
+#整合多个subplots
+import matplotlib.pyplot as plt
+
+fig, axs = plt.subplots(ncols=3, nrows=3)
+gs = axs[1, 2].get_gridspec()
+# remove the underlying axes
+for ax in axs[1:, -1]:
+    ax.remove()
+axbig = fig.add_subplot(gs[1:, -1])
+axbig.annotate('Big Axes \nGridSpec[1:, -1]', (0.1, 0.5),
+               xycoords='axes fraction', va='center')
+
+fig.tight_layout()
+
+plt.show()
